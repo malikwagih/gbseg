85abcf2a5e1d174a6ee38bf5acdfd6f23af7c95d
Your Name <mwagih@mit.edu>
2021-04-30 00:24:55 -0400

    upload page 1

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var solvent = document.getElementById("Solvent").value;
+    var solute = document.getElementById("Solute").value;
+    
+    var pic = "./js/"+solvent+solute+".jpg"; //"./js/"+
+    
+    function checkImage(imageSrc, good, bad) {
+        var img = new Image();
+        img.onload  = good; 
+        img.onerror = bad;
+        img.src = imageSrc;
+    }
+    
+    checkImage(pic, exist, not_exist);
+
+    // $.get(pic).done(exist).fail(not_exist)
+
+    function exist(){
+        // Displaying the value
+        document.getElementById("output").innerHTML = solvent+"-"+solute;
+        // 
+        document.getElementById('bigpic').style.display='block';
+        document.getElementById('bigpic').style.opacity=0.5;
+        document.getElementById('bigpic').src = pic; //.replace('90x90', '225x225');
+    }
+
+    function not_exist(){
+        document.getElementById("output").innerHTML = solvent+"-"+solute+" not available";
+    }
+}
+
+// let d = new Date();
+// document.getElementById("output").innerHTML = ""
